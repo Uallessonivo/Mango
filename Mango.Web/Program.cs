99971ee95227eb40757d4e3acd729a0bba59e336
Uallessonivo <uallessons@gmail.com>
2023-07-01 06:28:17 -0300

    wip: consuming the auth api

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -8,9 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddHttpClient();
-builder.Services.AddHttpClient<ICouponService, CouponService>();
+
 Sd.CouponAPIBase = builder.Configuration["ServiceUrls:CouponAPI"]!;
+Sd.AuthAPIBase = builder.Configuration["ServiceUrls:AuthAPI"]!;
+
+builder.Services.AddHttpClient<ICouponService, CouponService>();
+builder.Services.AddHttpClient<IAuthService, AuthService>();
+
 builder.Services.AddScoped<IBaseService, BaseService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<ICouponService, CouponService>();
 
 var app = builder.Build();
---
+++ Mango.Web/Service/AuthService.cs
@@ -0,0 +1,46 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Mango.Web.Utility;
+
+namespace Mango.Web.Service
+{
+    public class AuthService : IAuthService
+    {
+        private readonly IBaseService _baseService;
+
+        public AuthService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public async Task<ResponseDto?> AssignRoleAsync(RegistrationRequestDto registrationRequestDto)
+        {
+            return await _baseService.SendAsync(new RequestDto
+            {
+                ApiType = Sd.ApiType.POST,
+                Data = registrationRequestDto,
+                Url = Sd.AuthAPIBase + "/api/auth/AssignRole"
+            })!;
+        }
+
+        public async Task<ResponseDto?> LoginAsync(LoginRequestDto loginRequestDto)
+        {
+            return await _baseService.SendAsync(new RequestDto
+            {
+                ApiType = Sd.ApiType.POST,
+                Data = loginRequestDto,
+                Url = Sd.AuthAPIBase + "/api/auth/login"
+            })!;
+        }
+
+        public async Task<ResponseDto?> RegisterASync(RegistrationRequestDto registrationRequestDto)
+        {
+            return await _baseService.SendAsync(new RequestDto
+            {
+                ApiType = Sd.ApiType.POST,
+                Data = registrationRequestDto,
+                Url = Sd.AuthAPIBase + "/api/auth/register"
+            })!;
+        }
+    }
+}
