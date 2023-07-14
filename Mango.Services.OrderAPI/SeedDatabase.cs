d274a18b0b97d6c5157afc56bae47fff04d91088
Uallessonivo <uallessons@gmail.com>
2023-07-14 14:08:01 -0300

    wip: order api

---
+++ Mango.Services.OrderAPI/SeedDatabase.cs
@@ -0,0 +1,20 @@
+﻿using Mango.Services.OrderAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.OrderAPI
+{
+    public static class SeedDatabase
+    {
+        public static void ApplyMigration(WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+
+            var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+            if (db.Database.GetPendingMigrations().Any())
+            {
+                db.Database.Migrate();
+            }
+        }
+    }
+}
